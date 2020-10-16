e6d165bb99e6ad064704e6560137089fa198c7c9
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2020-10-16 20:14:18 +0800

    import `github.com/IceCodeNew/bash_script_template`

--- getting_start.sh
+++ getting_start.sh
@@ -1,39 +1,69 @@
 #!/bin/bash
 
+# IMPORTANT!
+# `apt` does not have a stable CLI interface. Use with caution in scripts.
+## Refer: https://askubuntu.com/a/990838
+export DEBIAN_FRONTEND=noninteractive
+
+## Refer: https://unix.stackexchange.com/a/356759
+export PAGER='cat'
+export SYSTEMD_PAGER=cat
+
+cd() {
+  cd "$@" || exit 1
+}
+rm() {
+  $(type -P rm) "$@"
+}
+cp() {
+  $(type -P cp) "$@"
+}
+mv() {
+  $(type -P mv) "$@"
+}
+mkdir() {
+  $(type -P mkdir) -p "$@"
+}
+curl() {
+  $(type -P curl) -LRq --retry 5 --retry-delay 10 --retry-max-time 60 "$@"
+}
+
+################
+
 sudo mkdir -p /usr/local/bin
 go_collection_tag_name=$(curl -sSL -H "Accept: application/vnd.github.v3+json" \
   'https://api.github.com/repos/icecodenew/go-collection/releases/latest' |
   grep 'tag_name' | cut -d\" -f4)
 sudo rm '/usr/local/bin/croc'
-curl -LR4q "https://github.com/IceCodeNew/go-collection/releases/download/${go_collection_tag_name}/croc" -o '/usr/local/bin/croc' &&
+curl "https://github.com/IceCodeNew/go-collection/releases/download/${go_collection_tag_name}/croc" -o '/usr/local/bin/croc' &&
   sudo chmod +x '/usr/local/bin/croc'
 sudo rm '/etc/bash_completion.d/croc'
-curl -sSLR4q 'https://github.com/schollz/croc/raw/master/src/install/bash_autocomplete' -o '/etc/bash_completion.d/croc'
+curl -sS 'https://github.com/schollz/croc/raw/master/src/install/bash_autocomplete' -o '/etc/bash_completion.d/croc'
 
 sudo rm '/usr/local/bin/shfmt'
-curl -LR4q "https://github.com/IceCodeNew/go-collection/releases/download/${go_collection_tag_name}/shfmt" -o '/usr/local/bin/shfmt' &&
+curl "https://github.com/IceCodeNew/go-collection/releases/download/${go_collection_tag_name}/shfmt" -o '/usr/local/bin/shfmt' &&
   sudo chmod +x '/usr/local/bin/shfmt'
 
 sudo rm '/usr/local/bin/github-release'
-curl -LR4q "https://github.com/IceCodeNew/go-collection/releases/download/${go_collection_tag_name}/github-release" -o '/usr/local/bin/github-release' &&
+curl "https://github.com/IceCodeNew/go-collection/releases/download/${go_collection_tag_name}/github-release" -o '/usr/local/bin/github-release' &&
   sudo chmod +x '/usr/local/bin/github-release'
 
 sudo rm '/usr/local/bin/go-shadowsocks2'
-curl -LR4q "https://github.com/IceCodeNew/go-collection/releases/download/${go_collection_tag_name}/go-shadowsocks2" -o '/usr/local/bin/go-shadowsocks2' &&
+curl "https://github.com/IceCodeNew/go-collection/releases/download/${go_collection_tag_name}/go-shadowsocks2" -o '/usr/local/bin/go-shadowsocks2' &&
   sudo chmod +x '/usr/local/bin/go-shadowsocks2'
 
 sudo rm '/usr/local/bin/nali'
-curl -LR4q "https://github.com/IceCodeNew/go-collection/releases/download/${go_collection_tag_name}/nali" -o '/usr/local/bin/nali' &&
+curl "https://github.com/IceCodeNew/go-collection/releases/download/${go_collection_tag_name}/nali" -o '/usr/local/bin/nali' &&
   sudo chmod +x '/usr/local/bin/nali'
 
-curl -LR4q -o 'caddy_linux_amd64.deb' \
+curl -o 'caddy_linux_amd64.deb' \
   "$(curl -sSL -H 'Accept: application/vnd.github.v3+json' \
     'https://api.github.com/repos/caddyserver/caddy/releases/latest' |
     grep 'browser_download_url' | grep 'linux_amd64.deb' | cut -d\" -f4)"
 sudo gdebi -n 'caddy_linux_amd64.deb' && rm 'caddy_linux_amd64.deb'
 sudo systemctl disable --now caddy
 sudo rm '/usr/bin/caddy' '/usr/local/bin/caddy' '/usr/local/bin/xcaddy' '/usr/local/bin/caddy-maxmind-geolocation'
-curl -LR4q "https://github.com/IceCodeNew/go-collection/releases/download/${go_collection_tag_name}/caddy-maxmind-geolocation" -o '/usr/bin/caddy' &&
+curl "https://github.com/IceCodeNew/go-collection/releases/download/${go_collection_tag_name}/caddy-maxmind-geolocation" -o '/usr/bin/caddy' &&
   sudo chmod +x '/usr/bin/caddy'
 
 sudo apt-get update
@@ -41,7 +71,7 @@ sudo apt-get -y install minify
 (
   tmp_dir=$(mktemp -d)
   cd "$tmp_dir" || exit 1
-  curl -LR4q \
+  curl \
     "$(curl -sSL -H 'Accept: application/vnd.github.v3+json' \
       'https://api.github.com/repos/tdewolff/minify/releases/latest' |
       grep 'browser_download_url' | grep 'linux_amd64.tar.gz' | cut -d\" -f4)" |
