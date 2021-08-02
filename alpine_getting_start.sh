ed631feccf75c7dbb1ae05277804be2cd4d61189
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2021-08-02 21:24:36 +0800

    Bug fix
    Signed-off-by: IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>

--- alpine_getting_start.sh
+++ alpine_getting_start.sh
@@ -3,11 +3,11 @@
 #
 # --- Script Version ---
 # Name    : alpine_getting_start.sh
-# Version : c28581a (1 commit after this ref)
+# Version : 5512f20 (1 commit after this ref)
 # Author  : IceCodeNew
 # Date    : March 2021
 # Download: https://raw.githubusercontent.com/IceCodeNew/go-collection/master/alpine_getting_start.sh
-readonly local_script_version='c28581a'
+readonly local_script_version='5512f20'
 
 curl_path="$(type -P curl)"
 # geo_country="$(curl 'https://api.myip.la/en?json' | jq . | grep country_code | cut -d'"' -f4)"
@@ -146,21 +146,21 @@ install_binaries() {
   if [[ x"$(echo "${install_github_release:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/github-release" '/usr/local/bin/github-release'
   else
-    rm '/usr/local/bin/github-release'
+    sudo rm '/usr/local/bin/github-release'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_nfpm:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/nfpm" '/usr/local/bin/nfpm'
   else
-    rm '/usr/local/bin/nfpm'
+    sudo rm '/usr/local/bin/nfpm'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_mosdns:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/mosdns" '/usr/local/bin/mosdns'
   else
-    rm '/usr/local/bin/mosdns'
+    sudo rm '/usr/local/bin/mosdns'
   fi
 
   # shellcheck disable=SC2154
@@ -185,7 +185,7 @@ install_binaries() {
     popd || exit 1
     /bin/rm -rf "$tmp_dir"
   else
-    rm '/usr/local/bin/go-shadowsocks2' '/usr/local/bin/sslocal' '/usr/local/bin/ssmanager' '/usr/local/bin/ssserver' '/usr/local/bin/ssurl'
+    sudo rm '/usr/local/bin/go-shadowsocks2' '/usr/local/bin/sslocal' '/usr/local/bin/ssmanager' '/usr/local/bin/ssserver' '/usr/local/bin/ssurl'
   fi
 
   # shellcheck disable=SC2154
@@ -193,14 +193,14 @@ install_binaries() {
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/frpc" '/usr/local/bin/frpc'
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/frps" '/usr/local/bin/frps'
   else
-    rm '/usr/local/bin/frpc' '/usr/local/bin/frps'
+    sudo rm '/usr/local/bin/frpc' '/usr/local/bin/frps'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_chisel:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/chisel" '/usr/local/bin/chisel'
   else
-    rm '/usr/local/bin/chisel'
+    sudo rm '/usr/local/bin/chisel'
   fi
 
   curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/got" '/usr/local/bin/got'
@@ -209,14 +209,14 @@ install_binaries() {
   if [[ x"$(echo "${install_dive:=yes}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/dive" '/usr/local/bin/dive'
   else
-    rm '/usr/local/bin/dive'
+    sudo rm '/usr/local/bin/dive'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_duf:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/duf" '/usr/local/bin/duf'
   else
-    rm '/usr/local/bin/duf'
+    sudo rm '/usr/local/bin/duf'
   fi
 
   curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/dnslookup" '/usr/local/bin/dnslookup'
@@ -232,49 +232,49 @@ install_binaries() {
   if [[ x"$(echo "${install_websocat:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/rust-collection/releases/latest/download/websocat" '/usr/local/bin/websocat'
   else
-    rm '/usr/local/bin/websocat'
+    sudo rm '/usr/local/bin/websocat'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_just:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/rust-collection/releases/latest/download/just" '/usr/local/bin/just'
   else
-    rm '/usr/local/bin/just'
+    sudo rm '/usr/local/bin/just'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_desed:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/rust-collection/releases/latest/download/desed" '/usr/local/bin/desed'
   else
-    rm '/usr/local/bin/desed'
+    sudo rm '/usr/local/bin/desed'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_fnm:=yes}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/rust-collection/releases/latest/download/fnm" '/usr/local/bin/fnm'
   else
-    rm '/usr/local/bin/fnm'
+    sudo rm '/usr/local/bin/fnm'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_rsign:=yes}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/rust-collection/releases/latest/download/rsign" '/usr/local/bin/rsign'
   else
-    rm '/usr/local/bin/rsign'
+    sudo rm '/usr/local/bin/rsign'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_b3sum:=yes}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/rust-collection/releases/latest/download/b3sum" '/usr/local/bin/b3sum'
   else
-    rm '/usr/local/bin/b3sum'
+    sudo rm '/usr/local/bin/b3sum'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_nali:=yes}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/nali" '/usr/local/bin/nali'
   else
-    rm '/usr/local/bin/nali'
+    sudo rm '/usr/local/bin/nali'
   fi
 
   [[ -n "$(type -P apk)" ]] && curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/apk-file" '/usr/local/bin/apk-file'
@@ -290,77 +290,77 @@ install_binaries() {
     popd || exit 1
     /bin/rm -rf "$tmp_dir"
   else
-    rm '/usr/local/bin/age' '/usr/local/bin/age-keygen'
+    sudo rm '/usr/local/bin/age' '/usr/local/bin/age-keygen'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_mtg:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/mtg" '/usr/local/bin/mtg'
   else
-    rm '/usr/local/bin/mtg'
+    sudo rm '/usr/local/bin/mtg'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_wuzz:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/wuzz" '/usr/local/bin/wuzz'
   else
-    rm '/usr/local/bin/wuzz'
+    sudo rm '/usr/local/bin/wuzz'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_httpstat:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/httpstat" '/usr/local/bin/httpstat'
   else
-    rm '/usr/local/bin/httpstat'
+    sudo rm '/usr/local/bin/httpstat'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_wgcf:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/wgcf" '/usr/local/bin/wgcf'
   else
-    rm '/usr/local/bin/wgcf'
+    sudo rm '/usr/local/bin/wgcf'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_mmp_go:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/mmp-go" '/usr/local/bin/mmp-go'
   else
-    rm '/usr/local/bin/mmp-go'
+    sudo rm '/usr/local/bin/mmp-go'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_cloudflarest:=yes}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/CloudflareST" '/usr/local/bin/CloudflareST'
   else
-    rm '/usr/local/bin/CloudflareST'
+    sudo rm '/usr/local/bin/CloudflareST'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_netflix_verify:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/nf" '/usr/local/bin/nf'
   else
-    rm '/usr/local/bin/nf'
+    sudo rm '/usr/local/bin/nf'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_piknik:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/go-collection/releases/latest/download/piknik" '/usr/local/bin/piknik'
   else
-    rm '/usr/local/bin/piknik'
+    sudo rm '/usr/local/bin/piknik'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_boringtun:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/rust-collection/releases/latest/download/boringtun-linux-musl-x64" '/usr/local/bin/boringtun'
   else
-    rm '/usr/local/bin/boringtun'
+    sudo rm '/usr/local/bin/boringtun'
   fi
 
   # shellcheck disable=SC2154
   if [[ x"$(echo "${install_cfnts:=no}" | cut -c1)" = x'y' ]]; then
     curl_to_dest "https://github.com/IceCodeNew/rust-collection/releases/latest/download/cfnts" '/usr/local/bin/cfnts'
   else
-    rm '/usr/local/bin/cfnts'
+    sudo rm '/usr/local/bin/cfnts'
   fi
 
   ################
